0235dccdcd52cf6aac16f1d6289768d9162ca457
Adrian Marceau <adrian.marceau@gmail.com>
2024-05-27 14:59:25 -0400

    Updated sprite logic in MainScene and preloaded Banner code

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -6,8 +6,12 @@
 // ------------------------------------------------------------ //
 
 import MMRPG from '../shared/MMRPG.js';
+
+import SpritesUtility from '../utils/SpritesUtility.js';
 import ButtonsUtility from '../utils/ButtonsUtility.js';
 import PopupsUtility from '../utils/PopupsUtility.js';
+
+import MainBanner from '../components/Banner/MainBanner.js';
 
 export default class MainScene extends Phaser.Scene
 {
@@ -17,11 +21,13 @@ export default class MainScene extends Phaser.Scene
         super('Main');
 
         // Initialize MMRPG utility class objects
+        let SPRITES = new SpritesUtility(this);
         let POPUPS = new PopupsUtility(this);
         let BUTTONS = new ButtonsUtility(this);
 
         // Ensure MMRPG and utility objects are available to the entire class
         this.MMRPG = MMRPG;
+        this.SPRITES = SPRITES;
         this.POPUPS = POPUPS;
         this.BUTTONS = BUTTONS;
 
@@ -39,12 +45,19 @@ export default class MainScene extends Phaser.Scene
 
         // Pull in required object references
         let MMRPG = this.MMRPG;
+        let SPRITES = this.SPRITES;
         let BUTTONS = this.BUTTONS;
         let POPUPS = this.POPUPS;
-        BUTTONS.setScene(this);
-        POPUPS.setScene(this);
+        SPRITES.preload(this);
+        BUTTONS.preload(this);
+        POPUPS.preload(this);
 
         /* ... */
+
+        // Trigger post-preload methods for utility classes
+        SPRITES.afterPreload(this);
+        BUTTONS.afterPreload(this);
+        POPUPS.afterPreload(this);
 
     }
 
@@ -54,29 +67,39 @@ export default class MainScene extends Phaser.Scene
 
         // Pull in required object references
         let MMRPG = this.MMRPG;
+        let SPRITES = this.SPRITES;
         let POPUPS = this.POPUPS;
         let BUTTONS = this.BUTTONS;
+        SPRITES.create(this);
+        BUTTONS.create(this);
+        POPUPS.create(this);
 
         // Create the base canvas for which the rest of the game will be drawn
         var $canvas = this.add.image(0, 0, 'canvas');
         $canvas.setOrigin(0, 0);
 
+        // Predefine some sizing details about the scene
+        var bannerSize = 'full';
+        var bannerWidth = 750;
+        var bannerHeight = 184;
+        var subMenuHeight = 23;
+        var readyRoomHeight = bannerHeight - subMenuHeight;
+        if (bannerSize === 'small'){
+            bannerHeight = 124;
+            readyRoomHeight = bannerHeight - subMenuHeight;
+            }
 
         // READY ROOM MOCKUP (Banner Area)
 
-        var readyRoomExpanded = true;
-        var readyRoomHeight = readyRoomExpanded ? 161 : 101;
         var x = MMRPG.canvas.centerX, y = 15;
-        var $readyRoom = this.add.image(x, y, 'mockup_main_banner_ready-room_' + (readyRoomExpanded ? 'full' : 'small'));
+        var $readyRoom = this.add.image(x, y, 'mockup_main_banner_ready-room_' + bannerSize);
         $readyRoom.setOrigin(0.5, 0);
 
         // SUB MENU MOCKUP (Menu Area)
 
-        var subMenuHeight = 23;
         var x = MMRPG.canvas.centerX, y = y + readyRoomHeight;
         var $subMenu = this.add.image(x, y, 'mockup_main_banner_sub-menu');
         $subMenu.setOrigin(0.5, 0);
-
 
         // HOME CONTENT MOCKUP (Content Area)
 
@@ -84,8 +107,31 @@ export default class MainScene extends Phaser.Scene
         var $contentHome = this.add.image(x, y, 'mockup_main_content_home');
         $contentHome.setOrigin(0.5, 0);
 
+        // BANNER COMPONENT (Main Banner)
+
+        /*
+        // Draw the main banner and collect a reference to it
+        var x = 15, y = 15;
+        this.mainBanner = new MainBanner(this, x, y, {
+            fullsize: true
+            });
+        */
+
         // Run any debug code we need to
         this.debug();
+
+
+        // Trigger post-create methods for utility classes
+        SPRITES.afterCreate(this);
+        BUTTONS.afterCreate(this);
+        POPUPS.afterCreate(this);
+
+    }
+
+    update(time, delta) {
+
+        // Update the banner if needed
+        // this.mainBanner.update({ backgroundColor: 0x654321 });
 
     }
 
@@ -97,6 +143,7 @@ export default class MainScene extends Phaser.Scene
 
         // Pull in required object references
         let MMRPG = this.MMRPG;
+        let SPRITES = this.SPRITES;
         let POPUPS = this.POPUPS;
         let BUTTONS = this.BUTTONS;
 
