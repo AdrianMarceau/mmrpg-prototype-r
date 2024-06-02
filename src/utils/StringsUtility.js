a6817f83694ef63652bfc55b6ee34dc86424c960
Adrian Marceau <adrian.marceau@gmail.com>
2024-06-02 01:42:57 -0400

    Created very basic StringsUtility for making dynamic text

---
+++ src/utils/StringsUtility.js
@@ -0,0 +1,57 @@
+// ------------------------------------------------------------- //
+// MMRPG-PROTOTYPE-R: StringsUtility.js
+// String utility class for the MMRPG. This is a singleton class
+// with miscellaneous text-related methods for the game.
+// ------------------------------------------------------------ //
+
+import MMRPG from '../shared/MMRPG.js';
+
+import { GraphicsUtility as Graphics } from '../utils/GraphicsUtility.js';
+
+export class StringsUtility {
+
+    static getDefaults ()
+    {
+        return {
+            font: 'Open Sans',
+            size: 16,
+            color: '#ffffff',
+            align: 'center',
+            bitmapFont: 'megafont-white',
+            bitmapSize: 16,
+            bitmapSpacing: 20,
+            bitmapOrigin: 0.5
+            };
+    }
+
+    static addBitmapText (ctx, x, y, text, options)
+    {
+
+        let defaults = this.getDefaults();
+        text = text || '...';
+        options = options || {};
+        size = options.size || defaults.bitmapSize;
+        font = options.font || defaults.bitmapFont;
+        spacing = options.spacing || defaults.bitmapSpacing;
+        origin = options.origin || defaults.bitmapOrigin;
+        var $loadText = this.add.bitmapText(x, y, font, text, size);
+        $loadText.setLetterSpacing(spacing);
+        $loadText.setOrigin(origin);
+
+    }
+
+    static addPlainText (ctx, x, y, text, styles)
+    {
+
+        let defaults = this.getDefaults();
+        text = text || '...';
+        styles = styles || {};
+        styles.fontFamily = styles.fontFamily || defaults.font;
+        styles.fontSize = styles.fontSize || defaults.size + 'px';
+        styles.color = Graphics.returnPhaserColor(styles.color || defaults.color);
+        let $text = ctx.add.text(x, y, text, styles);
+        return $text;
+
+    }
+
+}
