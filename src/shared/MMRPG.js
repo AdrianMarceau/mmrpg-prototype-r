7300c108c9154ff3115e9f492b7f54bb223c8ebe
Adrian Marceau <adrian.marceau@gmail.com>
2024-06-04 13:58:23 -0400

    MMRPG-R Version 4.0.40

--- src/shared/MMRPG.js
+++ src/shared/MMRPG.js
@@ -11,7 +11,7 @@ const MMRPG = {
     title: 'Mega Man RPG: Prototype (Remake)',
     created: '2024-05-20',
     modified: '2024-06-04',
-    version: '4.0.38'
+    version: '4.0.40'
     };
 
 // Define the absolute base width and height for the game canvas
