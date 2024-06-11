a5d00813fa8d22fbdc47ac5d9da7aa983f3e612e
Adrian Marceau <adrian.marceau@gmail.com>
2024-06-11 01:17:26 -0400

    Added new method addFormattedText() to StringsUtility

--- src/utils/StringsUtility.js
+++ src/utils/StringsUtility.js
@@ -6,6 +6,8 @@
 
 import MMRPG from '../shared/MMRPG.js';
 
+import { GraphicsUtility as Graphics } from '../utils/GraphicsUtility.js';
+
 export class StringsUtility {
 
     static getDefaults ()
@@ -13,6 +15,7 @@ export class StringsUtility {
         return {
             font: 'Open Sans',
             size: 16,
+            line: 1.6,
             color: '#ffffff',
             align: 'center',
             bitmapFont: 'megafont-white',
@@ -46,6 +49,7 @@ export class StringsUtility {
         styles = styles || {};
         styles.fontFamily = styles.fontFamily || defaults.font;
         styles.fontSize = styles.fontSize || defaults.size + 'px';
+        styles.lineHeight = styles.lineHeight || defaults.line + 'em';
         styles.color = styles.color || defaults.color;
         let $text = ctx.add.text(x, y, text, styles);
         return $text;
@@ -201,6 +205,278 @@ export class StringsUtility {
         // Return the generated string blocks
         //console.log('stringBlocks:', stringBlocks);
         return stringBlocks;
+
+    }
+
+    // Define a function for adding formatted text to the screen at given coordinates and size
+    static addFormattedText (ctx, x, y, text, options)
+    {
+        //console.log('StringsUtility.addFormattedText() w/', x, y, text, options);
+
+        // Collect refs to required indexes
+        let typesIndex = MMRPG.Indexes.types || {};
+
+        // Compensate for any missing options
+        options = Object.assign({}, options) || {};
+        options.x = x || 0;
+        options.y = y || 0;
+        options.size = options.size || 8;
+        options.font = options.font || 'megafont-white';
+        options.margin = options.margin || 4;
+        options.width = options.width || 0;
+        options.height = options.height || 0;
+        options.color = options.color || '#ffffff';
+        options.border = options.border || false;
+        options.styles = Object.assign({}, this.getDefaults(), (options.styles || {}));
+        options.depth = options.depth || 0;
+        options.padding = options.padding || 0;
+        //console.log('options:', options);
+
+        // Create a container to hold all these items inside of
+        // and make it so objects outside the container are hidden
+        const maskGraphics = ctx.add.graphics();
+        maskGraphics.fillStyle(0x660022);
+        maskGraphics.fillRect(options.x, options.y, options.width, options.height);
+        maskGraphics.setVisible(false);
+        const textAreaMask = maskGraphics.createGeometryMask();
+        const textAreaContainer = ctx.add.container();
+        textAreaContainer.setMask(textAreaMask);
+        textAreaContainer.setDepth(options.depth);
+
+        // Create an array to hold all the elements
+        let textAreaElements = [];
+
+        // If a border was defined, make sure we draw one around the perimeter of the text
+        if (options.border) {
+            let borderSize = 1;
+            let borderColour = 0xff0000;
+            let $border = ctx.add.graphics();
+            $border.lineStyle(borderSize, borderColour);
+            $border.strokeRect(
+                (x + borderSize),
+                (y + borderSize),
+                (options.width - (borderSize * 2)),
+                (options.height - (borderSize * 2))
+            );
+            $border.setDepth(options.depth - 1);
+            $border.setAlpha(0.5);
+            textAreaContainer.add($border);
+            textAreaContainer.sort('depth');
+            textAreaElements.push({kind: 'border', elem: $border});
+        }
+
+        // Parse the formatted text into an array of objects
+        let parsedStrings = this.parseFormattedText(text);
+        //console.log('parsedStrings:', parsedStrings);
+
+        // First pass: pre-render all text off-screen to measure
+        let parsedStringObjects = [];
+        for (let i = 0; i < parsedStrings.length; i++) {
+            //console.log('%c' + '----------', 'color: magenta;');
+            //console.log('%c' + 'parsedStrings['+i+'] = "' + parsedStrings[i].text + '"', 'color: pink;');
+
+            let parsedString = parsedStrings[i];
+            let stringObject = {};
+            stringObject.text = parsedString.text;
+            stringObject.size = [0, 0];
+            stringObject.elems = [];
+
+            let $text = ctx.add.bitmapText(-9999, -9999, options.font, parsedString.text, options.size);
+            $text.setOrigin(0);
+            $text.setDepth(options.depth + 1 + (i * 2));
+            $text.setLetterSpacing(20);
+            stringObject.size = [Math.ceil($text.width), Math.ceil($text.height)];
+            stringObject.elems.push($text);
+            textAreaContainer.add($text);
+            textAreaContainer.sort('depth');
+            textAreaElements.push({kind: 'text', elem: $text});
+
+            // Apply styles to text
+            let tagKey = -1, typeKey = -1;
+            if (parsedString.styles) {
+
+                // Loop through and collect and tag styles
+                let panelTags = [];
+                for (let j = 0; j < parsedString.styles.length; j++) {
+                    let style = parsedString.styles[j];
+                    if (!style.kind || style.kind !== 'tag'){ continue; }
+                    tagKey++;
+                    panelTags.push(style.tag);
+                    }
+                //console.log('panelTags:', panelTags);
+
+                // Loop through and collect and type styles
+                let panelTypes = [];
+                for (let j = 0; j < parsedString.styles.length; j++) {
+                    let style = parsedString.styles[j];
+                    if (!style.kind || style.kind !== 'type'){ continue; }
+                    typeKey++;
+                    let typeInfo = typesIndex[style.type];
+                    //console.log(typeInfo.name + ' is type key ' + typeKey);
+                    panelTypes.push(typeInfo.token);
+                    }
+                //console.log('panelTypes:', panelTypes);
+
+                // If there were any types, we should draw a type panel (with two colors if dual-typed)
+                if (panelTypes.length > 0){
+                    let padding = 2;
+                    let panelOffsetX = -1 * padding;
+                    let panelOffsetY = 0;
+                    let panelWidth = stringObject.size[0] + (padding * 2);
+                    let panelHeight = $text.height;
+                    let panelDepth = $text.depth - 1;
+                    let $typePanel;
+                    let typePanelConfig = {
+                        x: -9999,
+                        y: -9999,
+                        offsetX: panelOffsetX,
+                        offsetY: panelOffsetY,
+                        width: panelWidth,
+                        height: panelHeight,
+                        depth: panelDepth,
+                        border: null,
+                        background: null,
+                        background2: null
+                        };
+                    if (panelTypes.length >= 2) {
+                        let typeInfo1 = typesIndex[panelTypes[0]];
+                        let typeInfo2 = typesIndex[panelTypes[1]];
+                        typePanelConfig.border = Graphics.returnHexColorString(typeInfo1.colour_dark);
+                        typePanelConfig.background = Graphics.returnHexColorString(typeInfo1.colour_light);
+                        typePanelConfig.background2 = Graphics.returnHexColorString(typeInfo2.colour_dark);
+                        } else if (panelTypes.length === 1){
+                        let typeInfo = typesIndex[panelTypes[0]];
+                        typePanelConfig.border = Graphics.returnHexColorString(typeInfo.colour_dark);
+                        typePanelConfig.background = Graphics.returnHexColorString(typeInfo.colour_light);
+                        typePanelConfig.background2 = Graphics.returnHexColorString(typeInfo.colour_dark);
+                        }
+                    $typePanel = Graphics.addInlineTypePanel(ctx, typePanelConfig);
+                    if ($typePanel.elems){
+                        for (let k = 0; k < $typePanel.elems.length; k++) {
+                            let $typePanelElem = $typePanel.elems[k];
+                            textAreaContainer.add($typePanelElem);
+                            }
+                        }
+                    stringObject.elems.push($typePanel);
+                    textAreaContainer.sort('depth');
+                    textAreaElements.push({kind: 'panel', elem: $typePanel});
+                    if (panelWidth > stringObject.size[0]){ stringObject.size[0] = panelWidth; }
+                    if (panelHeight > stringObject.size[1]){ stringObject.size[1] = panelHeight; }
+                    }
+
+            }
+
+            parsedStringObjects.push(stringObject);
+
+        }
+
+        // Second pass: position text on-screen
+        let skipNextWord = false;
+        let baseX = (x + options.padding);
+        let baseY = (y + options.padding);
+        let maxWidth = (options.width - (options.padding * 2))
+        let maxHeight = (options.height - (options.padding * 2));
+        let currentX = baseX;
+        let currentY = baseY;
+        for (let i = 0; i < parsedStringObjects.length; i++) {
+            let stringObject = parsedStringObjects[i];
+            //console.log('%c----------', 'color: green;');
+            //console.log('parsedStringObjects[', i, '] "' + stringObject.text + '" =', typeof stringObject);
+
+            // If we're skipping the next word, do so and reset the flag
+            if (skipNextWord){
+                skipNextWord = false;
+                continue;
+                }
+
+            let $elems = stringObject.elems;
+            for (let j = 0; j < $elems.length; j++) {
+                let $elem = $elems[j];
+                $elem.setPosition(currentX, currentY);
+                //console.log('Setting position of $elem['+j+']:', $elem, 'to', currentX, currentY);
+                //console.log('$elem['+j+'] position is now at:', $elem.x, $elem.y);
+            }
+
+            // Measure the width of the next word off-screen
+            let nextWordText = '';
+            let nextWordSize = 0;
+            if (typeof parsedStringObjects[i + 1] !== 'undefined') {
+                let nextStringObject = parsedStringObjects[i + 1];
+                nextWordSize = nextStringObject.size[0];
+                nextWordText = nextStringObject.text;
+            }
+
+            // Update the x and y for the next word
+            currentX += stringObject.size[0];
+            if ((currentX + nextWordSize) > (baseX + maxWidth)) {
+                currentX = baseX;
+                currentY += stringObject.size[1] + options.margin;
+                if (nextWordText === ' '){ skipNextWord = true; }
+            }
+
+        }
+
+        // Generate a return object for this collection of text elements
+        const $formattedText = {
+            x: options.x,
+            y: options.y,
+            width: options.width,
+            height: options.height,
+            container: textAreaContainer,
+            elements: textAreaElements,
+            parsedStrings: parsedStrings,
+            parsedStringObjects: parsedStringObjects,
+            options: options,
+            };
+
+        // Define some functions we can use for this
+        function getPosition(){
+            return {
+                x: $formattedText.x,
+                y: $formattedText.y
+                };
+            };
+        function setPosition(x, y){
+            //console.log('setPosition(', x, y, ')');
+            x = intFromString(x, options.x);
+            y = intFromString(y, options.y);
+            let xDiff = x - options.x;
+            let yDiff = y - options.y;
+            //console.log('-> setPosition(', x, y, ') w/ diff:', xDiff, yDiff);
+            options.x = x;
+            options.y = y;
+            $formattedText.x = x;
+            $formattedText.y = y;
+            // Move the text area container and mask alongside with it
+            //textAreaContainer.setPosition(x, y);
+            maskGraphics.setPosition(maskGraphics.x + xDiff, maskGraphics.y + yDiff);
+            // Move each of the elements relative to the amount moved here
+            for (let i = 0; i < textAreaElements.length; i++) {
+                let elemObj = textAreaElements[i];
+                //console.log('updating elem['+i+']:', elemObj.kind, elemObj);
+                elemObj.elem.setPosition(elemObj.elem.x + xDiff, elemObj.elem.y + yDiff);
+                }
+            };
+        function getSize(){
+            return {
+                width: $formattedText.width,
+                height: $formattedText.height
+                };
+            };
+        function intFromString(value, base = 0){
+            if (typeof value === 'string' && value.match(/^(\+\=|\-=)/)){
+                let dx = parseInt(value.replace(/^(\+\=|\-=)/, ''));
+                value = base + (value.match(/^\+=/) ? dx : -dx);
+                //console.log('re-calculated value:', value);
+                }
+            return value;
+            };
+        $formattedText.getPosition = getPosition;
+        $formattedText.setPosition = setPosition;
+        $formattedText.getSize = getSize;
+
+        // Return the formatted text with some additional methods for positioning and sizing
+        return $formattedText;
 
     }
 
