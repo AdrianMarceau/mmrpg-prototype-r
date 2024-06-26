0b3379163e01c9aaf8907cf8e44cdb56886d7d09
Adrian Marceau <adrian.marceau@gmail.com>
2024-06-26 12:00:21 -0400

    Moved pause-related code from index.html and into main.js

--- src/main.js
+++ src/main.js
@@ -40,4 +40,34 @@ var config = {
         },
     };
 
+// make it so clicking the game container triggers an alert
+let gameIsReady = false;
+let gameIsRunning = false;
+let gameIsClickable = false;
+let gameResumeCallback = null;
+window.toggleGameIsReady = () => { gameIsReady = !gameIsReady; window.refreshGameContainer(); };
+window.toggleGameIsRunning = () => { gameIsRunning = !gameIsRunning; window.refreshGameContainer();  };
+window.toggleGameIsClickable = () => { gameIsClickable = !gameIsClickable; };
+window.setGameResumeCallback = (callback) => { gameResumeCallback = callback; };
+window.refreshGameContainer = () => {
+    const gameContainer = document.getElementById('game-container');
+    if (gameIsReady){ gameContainer.classList.add('ready'); }
+    else { gameContainer.classList.remove('ready'); }
+    if (gameIsRunning){ gameContainer.classList.add('running'); gameContainer.classList.remove('paused'); }
+    else { gameContainer.classList.add('paused'); gameContainer.classList.remove('running'); }
+    };
+window.refreshGameContainer();
+document.getElementById('game-container').addEventListener('click', () => {
+    if (!gameIsClickable){ return false; }
+    //console.log('The game container was clicked! w/ gameIsReady:', gameIsReady, 'gameIsRunning:', gameIsRunning);
+    if (!gameIsReady){ return false; }
+    if (gameIsRunning){ return false; }
+    window.toggleGameIsRunning(true);
+    if (gameResumeCallback
+        && typeof gameResumeCallback === 'function'){
+        gameResumeCallback();
+        }
+    return true;
+});
+
 let game = new Phaser.Game(config);
